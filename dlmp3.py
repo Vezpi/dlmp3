cded8508ecd15b546140ea22fdbd63f31b7bfc19
Vezpi <etienne.girault@gmail.com>
2014-07-01 21:03:48 +0200

    Daemon and debug mode is now available

--- dlmp3.py
+++ dlmp3.py
@@ -1,26 +1,33 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
-import dlmp3
+from dlmp3 import launcher
 import argparse
 
 __author__ = "Vezpi"
 __version__ = "0.01.0"
 __license__ = "GPLv3"
 
-def launcher():
+def dlmp3_launcher():
     parser = argparse.ArgumentParser(description='Music MP3 downloader')
     parser.add_argument('-w', '--web', action='store_true', help='launch the web server')
-    parser.add_argument('-d', '--daemon', action='store_true', help='the server is launched as a daemon')
+    parser.add_argument('-d', '--daemon', action='store_true', help='NOT IMPLEMENTED the server is launched as a daemon')
+    parser.add_argument('-p', '--port', type=int, help='specify the port which the server will listen')
     parser.add_argument('-v','--version', action='version', version=__version__, help='print the version of the application')
+    parser.add_argument('-D','--debug', action='store_true', help='run in debug mode')
     args = parser.parse_args()
     if args.web:
-        dlmp3.cli = False
+        launcher.CLI = False
         if args.daemon:
-            dlmp3.daemon = True
-        dlmp3.launch_server()
+            launcher.DAEMON = True
+        if args.port:
+        	launcher.PORT = args.port
+        	print (launcher.PORT)
+        if args.debug:
+        	launcher.DEBUG = True
+        launcher.mode_server()
     else:
-        dlmp3.launch_terminal()
+        launcher.mode_terminal()
 
 if __name__ == '__main__':
-    launcher()
+    dlmp3_launcher()
